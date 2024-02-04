0bd9afbcdd6534ede3dfa18da3c3dc1d7a9990d0
Daniil <92243840+MacIT54@users.noreply.github.com>
2024-02-04 22:54:26 +0700

    Update Account.cs

--- Account.cs
+++ Account.cs
@@ -4,40 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
- class Account
+class Account
+{
+    private decimal _balance;
+    private List<INotifyer> _notifyers;
+
+    public Account()
     {
-        private decimal _balance;
-        private List<INotifyer> _notifyers;
-
-        public Account() {}
-
-        public Account(decimal balance) : this()
-        {
-            _balance = balance;
-            _notifyers = new List<INotifyer>();
+        _balance = 0;
+        _notifyers = new List<INotifyer>();
     }
 
-        public void AddNotifyer(INotifyer notifyer)
-        {
-            _notifyers.Add(notifyer);
-        }
 
-        public void ChangeBalance(decimal balance)
-        {
-            _balance = balance;
-            Notification();
-        }
+    public Account(decimal balance)
+    {
+        _balance = balance;
+        _notifyers = new List<INotifyer>();
+    }
 
-        public decimal GetBalance()
-        {
-            return _balance;
-        }
+    public void AddNotifyer(INotifyer notifyer)
+    {
+        _notifyers.Add(notifyer);
+    }
 
-        private void Notification()
+    public void ChangeBalance(decimal balance)
+    {
+        _balance = balance;
+        Notification();
+    }
+
+    private void Notification()
+    {
+        foreach (var notifyer in _notifyers)
         {
-            foreach (var notifyer in _notifyers)
-            {
-                notifyer.Notify(_balance);
-            }
+            notifyer.Notify(_balance);
         }
     }
+
+    public decimal GetBalance
+    {
+        get { return _balance; }
+    }
+}
